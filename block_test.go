65f0f54c8c895d0ca2aaab8fff1d8ee17441da22
zcheng9 <zcheng9@hawk.iit.edu>
2020-07-13 11:45:05 +0800

    Update block_test.go
    add instruction

--- block_test.go
+++ block_test.go
@@ -12,16 +12,18 @@ import (
 func TestBlockChain_GetProof(t *testing.T) {
 	bc := NewBlockChain()
 	length := 50000
-
+	//first creat a blockchain with length of 50000,you can change the length
 	for i := 0; i < length; i++ {
 		b := NewBlock(uint64(i), 2, big.NewInt(10000))
 		bc.InsertBlock(b)
 	}
-
+	
+	//generate proof 
 	start := time.Now()
 	proof := bc.GetProof()
 	fmt.Printf("size of blockchain:%d,size of proof:%d\n",length,len(proof.Elems))
-
+	
+	//verify proof with limited information
 	fmt.Println("gen proof cost:", time.Now().Sub(start))
 	start = time.Now()
 	pBlocks, err := mmr.VerifyRequiredBlocks(proof, RightDif)
